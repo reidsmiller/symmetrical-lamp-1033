8344b224932d96e7d69b64e68a77630c4b68e290
Reid Miller <reid.s.miller@gmail.com>
2023-04-10 11:46:23 -0600

    fix: Remove unused method from customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,11 +2,4 @@ class CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:customer_id])
   end
-
-  def update
-    if params[:commit] == "Add Item"
-      CustomerItem.add_item(params)
-    end
-    redirect_to "/customers/#{params[:customer_id]}"
-  end
 end
